c3fa5de25ddcbb17a9d8cc72e38a5d59f4c9c0e9
DangaRanga <jasonblue1626@gmail.com>
2021-02-22 19:56:21 -0500

    Updated csrf protection

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,17 @@
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
 from flask_mail import Mail
 from .config import Config
 
 
+
 app = Flask(__name__)
+
+
 
 app.config.from_object(Config)
 
+csrf = CSRFProtect(app)
 mail = Mail(app)
 
 from . import views
